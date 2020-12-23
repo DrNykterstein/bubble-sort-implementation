9363b45e78156005926316673b70715cdf7a0b29
Daniel Leota <46200971+DrNykterstein@users.noreply.github.com>
2020-12-22 23:37:22 -0400

    Ordenamiento por el Metodo Burbuja en JS

---
+++ bubbleSort.js
@@ -0,0 +1,21 @@
+let numero = [2,1,4,5];
+function bubbleSort(numero){
+    let flag;
+    for(let i = 0; i < numero.length; i++){
+        for(let j = 0; j < numero.length; j++){
+            if(numero[j] > numero[j+1]){
+                flag = numero[j];
+                numero[j] = numero[j+1];
+                numero[j+1] = flag;
+            }
+        }
+    }
+}
+function viewArray(numero){
+    for(let k = 0; k < numero.length; k++){
+        console.log(numero[k]);
+    }
+}
+bubbleSort(numero);
+console.log("El arreglo ordenado es:");
+viewArray(numero);
