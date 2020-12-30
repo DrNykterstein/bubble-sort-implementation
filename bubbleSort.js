991f1a8ed824389687c9aad546a30ce168ca2713
Daniel Leota <46200971+DrNykterstein@users.noreply.github.com>
2020-12-29 20:48:27 -0400

    Pequeña mejora a nivel de coste

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,15 +1,20 @@
 let numero = [2,1,4,5];
 function bubbleSort(numero){
     let flag;
-    for(let i = 0; i < numero.length; i++){
-        for(let j = 0; j < numero.length; j++){
-            if(numero[j] > numero[j+1]){
+    let ordenado = false;
+    while(!(ordenado)){
+        ordenado = true;
+        for(let i = 0; i < numero.length; i++){
+            for(let j = 0; j < numero.length; j++){
+                if(numero[j] > numero[j+1]){
                 flag = numero[j];
                 numero[j] = numero[j+1];
                 numero[j+1] = flag;
+                }
             }
         }
     }
+    
 }
 function viewArray(numero){
     for(let k = 0; k < numero.length; k++){
